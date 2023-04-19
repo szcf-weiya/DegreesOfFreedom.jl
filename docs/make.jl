0490ce758bc909071e7c9f3b29b22a3be9493a98
szcf-weiya <szcfweiya@gmail.com>
2023-04-19 00:02:16 -0400

    deploy docs & no need ppa for pdftk on 22.04

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,7 @@ ENV["GKSwstype"] = "100"
 using Documenter, DegreesOfFreedom
 
 makedocs(sitename = "DegreesOfFreedom.jl Documentation")
+
+deploydocs(
+    repo = "github.com/szcf-weiya/DegreesOfFreedom.jl.git",
+)
