74f3d47173ae32b65e470f5c21b873f57a6e1951
szcf-weiya <szcfweiya@gmail.com>
2024-04-26 13:27:46 -0400

    wrap r scripts into a function for lasso vs subset

--- src/cpr_lasso_subset.R
+++ src/cpr_lasso_subset.R
@@ -1,47 +1,52 @@
 library(mvtnorm)
 library(glmnet)
 library(leaps)
-n = 20; p = 10; N = 100
-set.seed(20210319)
-#set.seed(999)
-Sigma = matrix(0, p, p)
-Sigma[1:4, 1:4] = 0.75
-Sigma[5:10, 5:10] = 0.9
-diag(Sigma) = 1
-X = rmvnorm(n, rep(0, p), Sigma)
-beta.true = c(1:5, rep(0, 5))
-y = X %*% beta.true + rnorm(n)
-lasso.fit = glmnet(X, y, alpha = 1, lambda = 10^seq(0.6, -2, length.out = 10))
-lasso.beta = as.matrix(lasso.fit$beta)
-lasso.beta
-apply(lasso.beta, 2, function(x) sum(x != 0))
-# s0 s1 s2 s3 s4 s5 s6 s7 s8 s9
-# 1  4  4  5  6  6  6  7  8 10
-lambda.grid = 10^seq(1, -2, length.out = 10)
-num_nonzeros = matrix(0, N, p*2)
-Y = matrix(0, nrow = n, ncol = N)
-Yhat = vector("list", 10*2)
-for (k in 1:20) Yhat[[k]] = matrix(0, nrow = n, ncol = N)
-for (i in 1:100) {
-  Y[,i] = X %*% beta.true + rnorm(n)
-  lasso.fit = glmnet(X, Y[,i], alpha = 1, lambda = lambda.grid, intercept = FALSE)
-  lasso.beta = as.matrix(lasso.fit$beta) # one column is for one lambda
-  num_nonzeros[i, 1:p] = apply(lasso.beta, 2, function(x) sum(x != 0))
-  bs.fit = regsubsets(X, Y[, i], intercept = FALSE, nvmax = 10)
-  bs.which = summary(bs.fit)$which # one row is for one subset
-  num_nonzeros[i, (p+1):(2*p)] = apply(bs.which, 1, sum)
-  for (k in 1:10) {
-    Yhat[[k]][, i] = X %*% lasso.beta[, k]
-    Yhat[[10+k]][, i] = as.matrix(X[, bs.which[k, ]]) %*% as.matrix(coef(bs.fit, k))
+lasso_vs_subset = function(N = 500, n = 20, p = 10, nlam = 10, seed = 20210319) {
+  set.seed(seed)
+  Sigma = matrix(0, p, p)
+  Sigma[1:4, 1:4] = 0.75
+  Sigma[5:10, 5:10] = 0.9
+  diag(Sigma) = 1
+  X = rmvnorm(n, rep(0, p), Sigma)
+  beta.true = c(1:5, rep(0, 5))
+  y = X %*% beta.true + rnorm(n)
+  
+  lambda.grid = 10^seq(1, -2, length.out = nlam)
+  num_nonzeros = matrix(0, N, p*2)
+  Y = matrix(0, nrow = n, ncol = N)
+  Yhat.subset = vector("list", p)
+  Yhat.lasso = vector("list", nlam)
+  n.non.subset = matrix(0, N, p)
+  n.non.lasso = matrix(0, N, nlam)
+  for (k in 1:p) Yhat.subset[[k]] = matrix(0, nrow = n, ncol = N)
+  for (k in 1:nlam) Yhat.lasso[[k]] = matrix(0, nrow = n, ncol = N)
+  for (i in 1:N) {
+    Y[,i] = X %*% beta.true + rnorm(n)
+    lasso.fit = glmnet(X, Y[,i], alpha = 1, lambda = lambda.grid, intercept = FALSE)
+    lasso.beta = as.matrix(lasso.fit$beta) # one column is for one lambda
+    n.non.lasso[i, ] = apply(lasso.beta, 2, function(x) sum(x != 0))
+    bs.fit = regsubsets(X, Y[, i], intercept = FALSE, nvmax = 10)
+    bs.which = summary(bs.fit)$which # one row is for one subset
+    n.non.subset[i, ] = apply(bs.which, 1, sum)
+    for (k in 1:p) {
+      Yhat.subset[[k]][, i] = as.matrix(X[, bs.which[k, ]]) %*% as.matrix(coef(bs.fit, k))
+    }
+    for (k in 1:nlam) {
+      Yhat.lasso[[k]][, i] = X %*% lasso.beta[, k]
+    }
   }
-}
-nns = apply(num_nonzeros, 2, mean)
-dfs = numeric(20)
-for (k in 1:20) {
-  dfs[k] = sum(sapply(1:n, function(i) cov(Y[i, ], Yhat[[k]][i, ])))
+  avg.n.non.lasso = apply(n.non.lasso, 2, mean)
+  avg.n.non.subset = apply(n.non.subset, 2, mean)
+  df.lasso = numeric(nlam)
+  df.subset = numeric(p)
+  for (k in 1:p) df.subset[k] = sum(sapply(1:n, function(i) cov(Y[i, ], Yhat.subset[[k]][i, ])))
+  for (k in 1:nlam) df.lasso[k] = sum(sapply(1:n, function(i) cov(Y[i, ], Yhat.lasso[[k]][i, ])))
+  list(n.lasso = avg.n.non.lasso, n.subset = avg.n.non.subset, df.lasso = df.lasso, df.subset = df.subset)
 }
 
-# plot(c(0,nns[1:10]), c(0,dfs[1:10]), xlab = "Average # of nonzero coefficients", ylab = "DoF", pch=1, col = "blue", type="o", xlim = c(0, 10), ylim = c(0, 10))
-# lines(c(0,nns[11:20]), c(0,dfs[11:20]), type = "o", pch = 2, col = "red")
-# abline(a=0, b=1, lty=2)
-# legend("bottomright", pch = c(1, 2), col = c("blue", "red"), legend = c("lasso", "best subset"))
+plot_lasso_vs_subset = function(n.lasso, n.subset, df.lasso, df.subset) {
+  plot(c(0, n.lasso), c(0, df.lasso), xlab = "Average # of nonzero coefficients", ylab = "DoF", pch=1, col = "blue", type="o", xlim = c(0, 10), ylim = c(0, 10))
+  lines(c(0, n.subset), c(0, df.subset), type = "o", pch = 2, col = "red")
+  abline(a = 0, b = 1, lty = 2)
+  legend("bottomright", pch = c(1, 2), col = c("blue", "red"), legend = c("lasso", "best subset"))
+}
--- src/subset.jl
+++ src/subset.jl
@@ -9,12 +9,15 @@ Run the experiment for comparing the degrees of freedom of lasso and best subset
 function run_experiment_lasso_vs_subset(; folder = "/tmp", #"../res/df"
                                         )
     R"source($file)"
-    nns = rcopy(R"nns")
-    dfs = rcopy(R"dfs")
-    plot(vcat(0, nns[1:10]), vcat(0, dfs[1:10]), xlab = "Average # of nonzero coefficients",
+    res = R"lasso_vs_subset()"
+    n_lasso = rcopy(R"$res$n.lasso")
+    n_subset = rcopy(R"$res$n.subset")
+    df_lasso = rcopy(R"$res$df.lasso")
+    df_subset = rcopy(R"$res$df.subset")
+    plot(vcat(0, n_lasso), vcat(0, df_lasso), xlab = "Average # of nonzero coefficients",
             ylab = "DoF", markershape = :star5, xlims = [0, 10.5], ylims = [0, 10.5], label = "Lasso", aspect_ratio = :equal, legend=:bottomright, size = (500, 500))
             # with pgfplotx, only aspect_ratio not work, need to add `size`
-    plot!(vcat(0, nns[11:20]), vcat(0, dfs[11:20]), markershape = :circle, label = "Best Subset")
+    plot!(vcat(0, n_subset), vcat(0, df_subset), markershape = :circle, label = "Best Subset")
     Plots.abline!(1, 0, label="", style=:dash)
     savefig(joinpath(folder, "df_lasso_subset.pdf"))
 end
